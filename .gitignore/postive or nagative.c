649fac796f5f4ed60dd2a48ad60101645623ece4
Crane <30362929+cranesingh@users.noreply.github.com>
2017-07-22 10:31:36 +0530

    Update and rename .gitignore to postive or nagative.c

---
+++ .gitignore/postive or nagative.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    {
+
+        printf("number is +ve");
+    }
+    else if(n<0)
+    {
+
+        printf("number is -ve");
+
+    }
+    else
+    {
+
+        printf("number is 0");
+    }
+    return 0;
+}
